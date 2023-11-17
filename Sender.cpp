fab4ed22c82d693959d460dc9da2be2ed93c09c7
Tikhon <tikhon.radk@gmail.com>
2023-11-17 09:28:07 +0300

    refactoring/fixes

--- Sender.cpp
+++ Sender.cpp
@@ -4,80 +4,108 @@
 #include <iostream>
 
 
+using std::cout;
+using std::cin;
+
+
+
+void processMessages(const std::string& file_name, HANDLE hStartEvent, HANDLE hInputReadySemaphore, HANDLE hOutputReadySemaphore, HANDLE hMutex);
+
 int main(int argc, char* argv[]) {
+    if (argc < 2) {
+        cout << "Usage: " << argv[0] << " <file_name>\n";
+        return 1;
+    }
+
     std::string file_name = argv[1];
     std::fstream file;
     HANDLE hStartEvent = OpenEvent(EVENT_MODIFY_STATE, FALSE, "Process Started");
-    if (hStartEvent == NULL)
-    {
-        std::cout << "Open event failed. \nInput any char to exit.\n";
-        std::cin.get();
+
+    if (hStartEvent == NULL) {
+        cout << "Open event failed. \nInput any char to exit.\n";
+        cin.get();
         return GetLastError();
     }
+
     HANDLE hInputReadySemaphore = OpenSemaphore(EVENT_ALL_ACCESS, FALSE, "Input Semaphore started");
     if (hInputReadySemaphore == NULL)
         return GetLastError();
+
     HANDLE hOutputReadySemaphore = OpenSemaphore(EVENT_ALL_ACCESS, FALSE, "Output Semaphore started");
     if (hOutputReadySemaphore == NULL)
         return GetLastError();
+
     HANDLE hMutex = OpenMutex(SYNCHRONIZE, FALSE, "mut ex");
+
     SetEvent(hStartEvent);
-    std::cout << "Event was started\n";
-    std::cout << "Input 1 to write message;\nInput 0 to exit process\n";
+    cout << "Event was started\n";
+
+    processMessages(file_name, hStartEvent, hInputReadySemaphore, hOutputReadySemaphore, hMutex);
+
+    CloseHandle(hInputReadySemaphore);
+    CloseHandle(hOutputReadySemaphore);
+    CloseHandle(hMutex);
+
+    return 0;
+}
+
+void processMessages(const std::string& file_name, HANDLE hStartEvent, HANDLE hInputReadySemaphore, HANDLE hOutputReadySemaphore, HANDLE hMutex) {
+    std::fstream file;
+    cout << "Print 1 to write message;\nInput 0 to exit process\n";
     int key;
-    std::cin >> key;
+    cin >> key;
+
     while (true) {
         if (key == 1) {
-            //WaitForSingleObject(hInputReadySemaphore, INFINITE);
             WaitForSingleObject(hMutex, INFINITE);
             file.open(file_name, std::ios::out | std::ios::app);
+
             std::string msg;
-            std::cout << "Input message to add\n";
-            std::cin >> msg;
+            cout << "Input message to add\n";
+            cin >> msg;
+
             char message[20];
-            for (int i = 0; i < msg.length(); i++)
-            {
+            for (int i = 0; i < msg.length(); i++) {
                 message[i] = msg[i];
             }
-            for (int i = msg.length(); i < 20; i++)
-            {
+
+            for (int i = msg.length(); i < 20; i++) {
                 message[i] = '\0';
             }
-            //ReleaseSemaphore(hInputReadySemaphore, 1, NULL);
+
             message[19] = '\n';
             ReleaseMutex(hMutex);
             ReleaseSemaphore(hOutputReadySemaphore, 1, NULL);
+
             if (ReleaseSemaphore(hInputReadySemaphore, 1, NULL) != 1) {
                 std::cout << "file is full";
                 ReleaseSemaphore(hOutputReadySemaphore, 1, NULL);
                 WaitForSingleObject(hOutputReadySemaphore, INFINITE);
                 ReleaseSemaphore(hOutputReadySemaphore, 1, NULL);
                 ReleaseSemaphore(hInputReadySemaphore, 1, NULL);
-                for (int i = 0; i < 20; i++)
-                {
+
+                for (int i = 0; i < 20; i++) {
                     file << message[i];
                 }
-                
-            }
-            else {
-                for (int i = 0; i < 20; i++)
-                {
+            } else {
+                for (int i = 0; i < 20; i++) {
                     file << message[i];
                 }
             }
+
             file.close();
-            std::cout << "\nInput 1 to write message;\nInput 0 to exit process\n";
-            std::cin >> key;
+            cout << "\nInput 1 to write message;\nInput 0 to exit process\n";
+            cin >> key;
         }
+
         if (key != 0 && key != 1) {
-            std::cout << "\nIncorrect value!\nInput 1 to write message;\nInput 0 to exit process\n";
-            std::cin >> key;
+            cout << "\nIncorrect value!\nInput 1 to write message;\nInput 0 to exit process\n";
+            cin >> key;
         }
+
         if (key == 0) {
-            std::cout << "Process ended.";
+            cout << "Process ended.";
             break;
         }
     }
-
-    return 0;
 }
